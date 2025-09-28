3dd52c22bf0a01f474bb9d85859bfbad5772b94f
krtScrtr44 <secretariokurt3219@gmail.com>
2025-09-28 15:20:24 +0800

    script: create a script to toggle password input from password to text and vice versa

---
+++ public/script/event/toggle-password.js
@@ -0,0 +1,38 @@
+const ICON_PATH = 'asset/image/icon/'
+
+const inputs = document.querySelectorAll(
+    '.password-toggle-wrapper > input[type="password"]',
+    '.password-toggle-wrapper > input[type="text"]'
+)
+
+function togglePassword(input) {
+    const icon = input.parentElement.querySelector(
+        '.password-toggle-wrapper > img'
+    )
+
+    function displayIcon() {
+        icon.style.display = 'inline-block';
+    }
+    function hideIcon() {
+        icon.style.display = 'none';
+    }
+
+    input.addEventListener('click', displayIcon)
+    input.addEventListener('mouseover', displayIcon)
+    input.addEventListener('mouseout', hideIcon)
+
+    icon.addEventListener('mouseover', displayIcon)
+    icon.addEventListener('mouseout', hideIcon)
+
+    icon.onclick = (e) => {
+        e.stopPropagation()
+        if (input.type === 'password') {
+            input.type = 'text'
+            icon.src = `${ICON_PATH}show_b.svg`
+        } else {
+            input.type = 'password'
+            icon.src = `${ICON_PATH}hide_b.svg`
+        }
+    }
+}
+inputs.forEach(input => togglePassword(input))
