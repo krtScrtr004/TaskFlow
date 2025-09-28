0768e20d99a82cef9393085875bae6dd02dea6ec
krtScrtr44 <secretariokurt3219@gmail.com>
2025-09-28 13:59:58 +0800

    config: add config files

---
+++ source/backend/config/config.php
@@ -0,0 +1,45 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+
+require_once __DIR__ . DS . 'path.php';
+
+require_once VENDOR_PATH . 'autoload.php';
+
+require_once __DIR__ . DS . 'env.php';
+
+spl_autoload_register(function ($class) {
+    $paths = [
+        CONTAINER_PATH,
+        CORE_PATH,
+        DEPENDENT_PATH,
+        DUMP_PATH,
+        ENTITY_PATH,
+        ENUM_PATH,
+        ROUTER_PATH,
+        MIDDLEWARE_PATH,
+        MODEL_PATH
+    ];
+    foreach ($paths as $path) {
+        // Turn camel case to kebab case
+        $class = camelToKebabCase($class);
+
+        $file = $path . DS . str_replace('\\', '/', $class) . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            break;
+        }
+    }
+});
+
+$paths = [
+    FE_UTILITY_PATH,
+    BE_UTILITY_PATH,
+    CONTROLLER_PATH,
+    FUNCTION_COMPONENT_PATH
+];
+foreach ($paths as $path) {
+    foreach (glob($path . '*.php') as $fileName) {
+        require_once $fileName;
+    }
+}
---
+++ source/backend/router/register-routes.php
@@ -0,0 +1,30 @@
+<?php
+
+try {
+    $router = Router::getRouter();
+
+    $fileNames = ['page-routes.json', 'backend-routes.json'];
+    foreach ($fileNames as $fileName) {
+        $routesFile = DATA_PATH . $fileName;
+        $routes = decodeData($routesFile);
+        foreach ($routes as $method => $route) {
+            foreach ($route as $path => $action) {
+                // Remove trailing forward slash
+                if ($path[strlen($path) - 1] === '/')
+                    $path = substr($path, 0, strlen($path) - 1);
+
+            /*
+            If action provided is an array:
+                - The first element is the controller class name
+                - The second is the method name that controls the page
+            If not, the default name for controlling method is 'index'
+            */
+                $actions = (is_array($action)) ? [$action[0], $action[1]] : [$action, 'index'];
+                $router->register($path, $method, $actions);
+            }
+        }
+    }
+} catch (\Throwable $error) {
+    // TODO
+    echo $error->getMessage();
+}
