56bde5cc00ecbcc945f0a41db45e6a13b203d7bb
krtScrtr44 <secretariokurt3219@gmail.com>
2025-09-29 09:37:23 +0800

    core: add toArray and fromArray interface methods on entity interface

--- source/backend/core/entity.php
+++ source/backend/core/entity.php
@@ -2,4 +2,8 @@
 
 interface Entity extends JsonSerializable {
     public function jsonSerialize(): array;
+
+    public function toArray(): array;
+
+    public static function fromArray(array $data): self;
 }
