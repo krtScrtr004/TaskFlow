794cbfdf3901329148b0eceaed28d0866ec52121
krtScrtr44 <secretariokurt3219@gmail.com>
2025-09-29 10:43:09 +0800

    change: add a static method in ProjectTaskStatus enum to determine the status from start and completion date

--- source/backend/enum/project-task-status.php
+++ source/backend/enum/project-task-status.php
@@ -1,6 +1,7 @@
 <?php
 
 enum ProjectTaskStatus: string {
+    case PENDING = 'pending';
     case ON_GOING = 'onGoing';
     case COMPLETED = 'completed';
     case DELAYED = 'delayed';
@@ -8,11 +9,26 @@ enum ProjectTaskStatus: string {
 
     public function getDisplayName(): string {
         return match($this) {
+            self::PENDING => ucwords(camelToSentenceCase(self::PENDING->value)),
             self::ON_GOING => ucwords(camelToSentenceCase(self::ON_GOING->value)),
             self::COMPLETED => ucwords(camelToSentenceCase(self::COMPLETED->value)),
             self::DELAYED => ucwords(camelToSentenceCase(self::DELAYED->value)),
             self::CANCELLED => ucwords(camelToSentenceCase(self::CANCELLED->value))
         };
+    }
+
+    public static function getStatusFromDates(DateTime $startDate, DateTime $endDate): ProjectTaskStatus {
+        $now = new DateTime();
+
+        if ($now < $startDate) {
+            return self::PENDING;
+        } elseif ($now >= $startDate && $now <= $endDate) {
+            return self::ON_GOING;
+        } elseif ($now > $endDate) {
+            return self::COMPLETED;
+        } else {
+            throw new Exception("Unable to determine status from given dates.");
+        }
     }
 
     public static function fromString(string $value): ProjectTaskStatus {
