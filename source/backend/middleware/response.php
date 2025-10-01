40bdf32851dbfba3c563f67038b404582ed59713
krtScrtr44 <secretariokurt3219@gmail.com>
2025-10-02 00:33:17 +0800

    middleware: create Response middleware class

---
+++ source/backend/middleware/response.php
@@ -0,0 +1,29 @@
+<?php
+
+class Response {
+    private function __construct() {}
+
+    public static function success($data, $message, $statusCode = 200): void {
+        self::sendResponse($statusCode, [
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+
+    public static function error($message, $statusCode = 400, $errors = []): void {
+        self::sendResponse($statusCode, [
+            'status' => 'error',
+            'message' => $message,
+            'errors' => $errors
+        ]);
+    }
+
+    private static function sendResponse($statusCode, $body) {
+        http_response_code($statusCode);
+        header('Content-Type: application/json');
+        ob_clean();
+        echo json_encode($body);
+        exit();
+    }
+}
