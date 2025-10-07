a3a4f2aad22d8e464c35dd66ed642bca4ae031f3
krtScrtr44 <secretariokurt3219@gmail.com>
2025-10-07 14:00:24 +0800

    func: add a debounce function for async functions

--- public/script/utility/debounce.js
+++ public/script/utility/debounce.js
@@ -8,3 +8,29 @@ export function debounce(callback, delay = 300) {
         }, delay)
     }
 }
+
+export function debounceAsync(fn, delay) {
+    let timer
+    let pendingReject
+
+    return function (...args) {
+        // Cancel previous pending Promise
+        if (pendingReject) {
+            pendingReject(new Error('Debounced'))
+        }
+
+        return new Promise((resolve, reject) => {
+            pendingReject = reject
+            clearTimeout(timer)
+
+            timer = setTimeout(async () => {
+                try {
+                    const result = await fn.apply(this, args)
+                    resolve(result)
+                } catch (err) {
+                    reject(err)
+                }
+            }, delay)
+        })
+    }
+}
