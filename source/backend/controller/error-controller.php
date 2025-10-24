62c03b6bfe7f125c7765cd9c85e29f58c8b4bc8e
krtScrtr44 <secretariokurt3219@gmail.com>
2025-10-24 17:55:02 +0800

    controller: create controller for error pages

---
+++ source/backend/controller/error-controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Interface\Controller;
+
+class ErrorController implements Controller
+{
+    public static function index(array $component = []): void
+    {
+        require_once VIEW_PATH . 'http-error.php';
+    }
+
+    public static function notFound(): void
+    {
+        self::index([
+            'title' => '404 Not Found',
+            'status' => '404'
+        ]);
+    }
+
+    public static function forbidden(): void
+    {
+        self::index([
+            'title' => '403 Forbidden',
+            'status' => '403'
+        ]); 
+    }
+}
