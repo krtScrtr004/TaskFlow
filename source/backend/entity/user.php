5d0510822ee7d393f2d01c9faa8e034320b3f59d
krtScrtr44 <secretariokurt3219@gmail.com>
2025-10-25 17:21:17 +0800

    change: unextend UserModel on User entity

--- source/backend/entity/user.php
+++ source/backend/entity/user.php
@@ -18,7 +18,7 @@ use DateTime;
 
 require_once ENUM_PATH . 'role.php';
 
-class User extends UserModel implements Entity
+class User implements Entity
 {
     private ?int $id;
     private ?UUID $publicId;
