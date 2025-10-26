d0e8b3354157184862f4fd93be275af6eaaa0dfa
krtScrtr44 <secretariokurt3219@gmail.com>
2025-10-26 10:53:53 +0800

    change: move path.php to data folder

--- source/backend/config/config.php
+++ source/backend/config/config.php
@@ -3,9 +3,9 @@
 define('STRICT_TYPES', 1);
 define('DS', '/');
 
-require_once __DIR__ . DS . 'path.php';
-
+require_once dirname(__DIR__, 1) . DS . 'data' . DS . 'path.php';
 require_once VENDOR_PATH . 'autoload.php';
+require_once DATA_PATH . 'data-length.php';
 
 // require_once __DIR__ . DS . 'env.php';
 
