694f4581dc00aebbea6ca4fc7b7a7815158e594d
krtScrtr44 <secretariokurt3219@gmail.com>
2025-11-23 22:19:41 +0800

    db: add event schduler database file

--- database/taskflow.sql
+++ database/taskflow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2025 at 06:40 AM
+-- Generation Time: Nov 23, 2025 at 03:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -90,7 +90,7 @@ CREATE TABLE `phasetaskworker` (
   `status` varchar(20) NOT NULL,
   `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
   `updatedAt` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
