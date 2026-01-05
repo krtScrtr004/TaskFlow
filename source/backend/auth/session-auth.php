adc8273cb1a74aac87be6562e6c50292a1c24d06
krtScrtr004 <secretariokurt3219@gmail.com>
2026-01-05 14:21:41 +0800

    change: change parameter types accepted by setAuthorizedSession in SessionAuth class

--- source/backend/auth/session-auth.php
+++ source/backend/auth/session-auth.php
@@ -4,9 +4,8 @@ namespace App\Auth;
 
 use App\Core\Me;
 use App\Core\Session;
-use App\Core\UUID;
-use App\Entity\User;
-use App\Middleware\Csrf;
+use App\Dependent\ProjectManager;
+use App\Dependent\Worker;
 
 class SessionAuth
 {
@@ -17,7 +16,7 @@ class SessionAuth
         return (Me::getInstance() !== null) && Session::isSet() && Session::has('userData');
     }
 
-    public static function setAuthorizedSession(User|array $user): void
+    public static function setAuthorizedSession(ProjectManager|Worker|array $user): void
     {
         // Always ensure session is started FIRST
         if (!Session::isSet()) {
@@ -30,22 +29,6 @@ class SessionAuth
 
         // Store user data in session
         $currentUser = Me::getInstance();
-        Session::set('userData', [
-            'id' => $currentUser->getId(),
-            'publicId' => UUID::toString($currentUser->getPublicId()),
-            'firstName' => $currentUser->getFirstName(),
-            'middleName' => $currentUser->getMiddleName(),
-            'lastName' => $currentUser->getLastName(),
-            'gender' => $currentUser->getGender()->value,
-            'birthDate' => $currentUser->getBirthDate()?->format('Y-m-d'),
-            'role' => $currentUser->getRole()->value,
-            'jobTitles' => implode(',', $currentUser->getJobTitles()->toArray()),
-            'contactNumber' => $currentUser->getContactNumber(),
-            'email' => $currentUser->getEmail(),
-            'bio' => $currentUser->getBio(),
-            'profileLink' => $currentUser->getProfileLink(),
-            'createdAt' => $currentUser->getCreatedAt()->format('Y-m-d H:i:s'),
-            'additionalInfo' => $currentUser->getAdditionalInfo()
-        ]);
+        Session::set('userData', $currentUser->toArray());
     }
 }
